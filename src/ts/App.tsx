c9ad3fcaba929916e687ba0acecd6e29e1b88daf
helloShen <symantec__@hotmail.com>
2022-05-12 13:30:00 -0400

    test route

--- src/ts/App.tsx
+++ src/ts/App.tsx
@@ -54,14 +54,10 @@ const App: React.FC = () => {
         <Router>
           <Nav purchaseList={purchaseList} />
           <Routes>
-            <Route path="" element={<Home />} />
-            <Route path="/" element={<Home />} />
-            <Route path="/index.html" element={<Home />} />
             <Route path="/shopping-app" element={<Home />} />
-            <Route path="/shopping-app/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
+            <Route path="/shopping-app/home" element={<Home />} />
             <Route
-              path="/shopping"
+              path="/shopping-app/shopping"
               element={
                 <Shopping
                   products={products}
@@ -69,7 +65,7 @@ const App: React.FC = () => {
                   loading={loading}
                 />}
             />
-            <Route path="/cart" element={
+            <Route path="/shopping-app/cart" element={
               <Cart
                 purchaseList={purchaseList}
                 purchaseDispatch={purchaseListDispatch} />
